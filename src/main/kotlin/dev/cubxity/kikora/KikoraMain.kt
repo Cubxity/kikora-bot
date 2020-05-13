9f8af17141892d5d055da3981b899c8af23b57f0
Cubxity <contact@cubxity.dev>
2020-05-13 17:03:08 +0200

    made sessionId required

--- src/main/kotlin/dev/cubxity/kikora/KikoraMain.kt
+++ src/main/kotlin/dev/cubxity/kikora/KikoraMain.kt
@@ -2,58 +2,54 @@ package dev.cubxity.kikora
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import dev.cubxity.kikora.utils.filterLeaf
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import kotlin.system.exitProcess
 
-object KikoraMain : CliktCommand(), KikoraBot.Listener {
+object KikoraMain : CliktCommand(name = "kikora-bot"), KikoraBot.Listener {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    private val sessionId: String? by option(help = "Kikora's JSESSIONID")
+    private val sessionId: String by option(help = "Kikora's JSESSIONID").required()
     private val tile: String? by option(help = "Tile to run the bot on")
     private val container: String? by option(help = "Container to run the bot on")
 
     override fun run() = runBlocking {
-        val sessionId = sessionId
-        if (sessionId != null) {
-            val api = KikoraAPI(sessionId)
-            val bot = KikoraBot(api)
+        val api = KikoraAPI(sessionId)
+        val bot = KikoraBot(api)
 
-            logger.info("Fetching tiles...")
-            val tiles = api.tileSet().tiles
+        logger.info("Fetching tiles...")
+        val tiles = api.tileSet().tiles
 
-            val tileId = tile
-            val containerId = container
+        val tileId = tile
+        val containerId = container
 
-            if (tileId != null) {
-                logger.info("Fetching exercises...")
-                val tile = tiles.find { it.id == tileId }
-                if (tile != null) {
-                    if (containerId != null) {
-                        val container = api.containerSet(tile.containers.first().containerId).containers.map { it.containerContent }
-                                .filterLeaf().toList()
-                                .find { it.containerId == containerId }
-                        if (container != null) {
-                            bot.execute(container, this@KikoraMain).join()
-                        } else {
-                            logger.error("Unable to find container with id $containerId")
-                            exitProcess(1)
-                        }
+        if (tileId != null) {
+            logger.info("Fetching exercises...")
+            val tile = tiles.find { it.id == tileId }
+            if (tile != null) {
+                if (containerId != null) {
+                    val container = api.containerSet(tile.containers.first().containerId).containers.map { it.containerContent }
+                            .filterLeaf().toList()
+                            .find { it.containerId == containerId }
+                    if (container != null) {
+                        bot.execute(container, this@KikoraMain).join()
                     } else {
-                        val containers = api.containerSet(tile.containers.first().containerId).containers.map { it.containerContent }
-                                .filterLeaf().toList()
-                        bot.execute(containers, this@KikoraMain).join()
+                        logger.error("Unable to find container with id $containerId")
+                        exitProcess(1)
                     }
                 } else {
-                    logger.error("Unable to find tile with id $tileId")
-                    exitProcess(1)
+                    val containers = api.containerSet(tile.containers.first().containerId).containers.map { it.containerContent }
+                            .filterLeaf().toList()
+                    bot.execute(containers, this@KikoraMain).join()
                 }
             } else {
-                logger.error("Tile not specified")
+                logger.error("Unable to find tile with id $tileId")
+                exitProcess(1)
             }
         } else {
-            // TODO: GUI
+            logger.error("Tile not specified")
         }
     }
 
